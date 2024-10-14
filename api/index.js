8d33ce104674e850ba8a577707d8fef449223fa7
Bozhidar Kumanov <67313433+bojo509@users.noreply.github.com>
2024-10-14 22:36:26 +0300

    Added database func

--- api/index.js
+++ api/index.js
@@ -1,12 +1,39 @@
 import express from 'express'
+import pg from 'pg';
+import { sql } from "@vercel/postgres";
+import dotenv from 'dotenv'
 
 const app = express()
 const port = process.env.PORT || 3000
-app.get('/', (req, res) => {
+const { Pool } = pg;
+
+dotenv.config()
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL
+})
+
+pool.connect((error) => {
+    if (error) {
+        console.log(error)
+    }
+})
+
+app.get('/static-links', (req, res) => {
     res.json([
-        {"link": "https://parfium.bg/mont-blanc-legend-blue-parfyumna-voda-za-mazhe-bez-opakovka-edp", "title": "montblanc legend blue without box"},
-        {"link": "https://parfium.bg/mont-blanc-legend-blue-parfyumna-voda-za-mazhe-edp", "title": "montblanc legend blue"}
+        { "link": "https://parfium.bg/mont-blanc-legend-blue-parfyumna-voda-za-mazhe-bez-opakovka-edp", "title": "montblanc legend blue without box" },
+        { "link": "https://parfium.bg/mont-blanc-legend-blue-parfyumna-voda-za-mazhe-edp", "title": "montblanc legend blue" }
     ])
+})
+
+app.get('/', async (req, res) => {
+    try {
+        const { rows } = await sql`SELECT link, title FROM links`; // Log the result of the query
+        res.json(rows);
+    } catch (error) {
+        console.error('Error fetching links:', error); // Log any errors
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 })
 
 app.listen(port, () => {
